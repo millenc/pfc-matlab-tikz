05e36af552d2a8253e2edb4c73807088323ff197
Mikel Pintor <millen@gmail.com>
2014-05-04 15:51:54 +0200

    Añadidas gráfica de ejemplo y función de pertenencia del ejemplo altura muy alta. Añadidos los ficheros .asv al .gitignore.

---
+++ example_fuzzyset_mf.m
@@ -0,0 +1,24 @@
+x=150:0.1:230;
+y=sigmf(x,[0.4 190]);
+i=451;
+
+% Parámetros para mejorar la visualización de datos
+width = 3;     % Width in inches
+height = 3;    % Height in inches
+alw = 0.75;    % AxesLineWidth
+fsz = 11;      % Fontsize
+lw = 1.5;      % LineWidth
+msz = 8;       % MarkerSize
+
+figure(2);
+pos = get(gcf, 'Position');
+%set(gcf, 'Position', [pos(1) pos(2) width*100, height*100]); %<- Set size
+set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+plot(x,y,'b-',[195],[0.88],'g*','LineWidth',lw,'MarkerSize',msz); %<- Specify plot properites
+xlim([150 230]);
+ylim([-0.1 1.1]);
+legend('\mu_{VT}(\mu_{i})', 'Location', 'SouthEast');
+xlabel('\mu_{i} - Altura en cm');
+ylabel('\mu_{VT}(\mu_{i})');
+text(x(i), 0.88, ['  \leftarrow (\mu_{i}=', num2str(x(i)), '; \mu_{VT}(195)=', num2str(y(i)), ')']);
+matlab2tikz('very-tall-fuzzyset-example.tikz','showInfo', false,'standalone', false,'height', '\figureheight', 'width', '\figurewidth');
---
+++ figure_example.m
@@ -0,0 +1,23 @@
+f = @(x) x.^2;
+g = @(x) 5*sin(x)+5;
+dmn = -pi:0.001:pi;
+xeq = dmn(abs(f(dmn) - g(dmn)) < 0.002);
+
+% Defaults for this blog post
+width = 3;     % Width in inches
+height = 3;    % Height in inches
+alw = 0.75;    % AxesLineWidth
+fsz = 11;      % Fontsize
+lw = 1.5;      % LineWidth
+msz = 8;       % MarkerSize
+
+figure(2);
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1) pos(2) width*100, height*100]); %<- Set size
+set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+plot(dmn,f(dmn),'b-',dmn, g(dmn),'r--',xeq,f(xeq),'g*','LineWidth',lw,'MarkerSize',msz); %<- Specify plot properites
+xlim([-pi pi]);
+legend('f(x)', 'g(x)', 'f(x)=g(x)', 'Location', 'SouthEast');
+xlabel('x');
+title('Improved Example Figure');
+matlab2tikz('publication-quality-example.tikz','showInfo', false,'standalone', false,'height', '\figureheight', 'width', '\figurewidth');
