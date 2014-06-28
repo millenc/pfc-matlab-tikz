921463bb921f990ffc6a7f597361910de95829d7
Mikel Pintor <millen@gmail.com>
2014-06-28 19:38:59 +0200

    Añadida función de solapamiento (raíz cuadrada del producto).

--- overlapfunctions.m
+++ overlapfunctions.m
@@ -12,6 +12,15 @@ ylabel('y');
 zlabel('G_{O}(x,y)');
 matlab2tikz('./output/overlap-min-3dplot.tikz','showInfo', false,'standalone', false,'height', '\figureheight', 'width', '\figurewidth');
 
+%sqrt
+M = sqrt(X.*Y);
+figure;
+surf(X,Y,M);
+xlabel('x');
+ylabel('y');
+zlabel('G_{O}(x,y)');
+matlab2tikz('./output/overlap-sqrt-3dplot.tikz','showInfo', false,'standalone', false,'height', '\figureheight', 'width', '\figurewidth');
+
 %sqrt y max(1-x,1-y)
 M = sqrt(X.*Y)./(sqrt(X.*Y) + max(1-X,1-Y));
 figure;
